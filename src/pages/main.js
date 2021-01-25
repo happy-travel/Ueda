d3f5c930069d4a86ec6ba0b643e29113f8d86703
kuskido <vsevolod.petrushchenko@happytravel.com>
2021-01-25 18:08:46 +0300

    invitation ready

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { Link } from 'react-router-dom';
+import { getInvite, forgetInvite } from 'matsumoto/src/core/auth/invite';
+import { API } from 'matsumoto/src/core';
+import apiMethods from 'core/methods';
 
 @observer
 class UedaMainPage extends React.Component {
+    componentDidMount() {
+        const invitationCode = getInvite();
+        if (invitationCode) {
+            API.post({
+                url: apiMethods.adminRegister,
+                body: invitationCode,
+                success: () => {
+                    forgetInvite();
+                    alert('Registered');
+                    this.setState({
+                        redirect: '/'
+                    });
+                },
+                error: (error) => {
+                    forgetInvite();
+                    alert(JSON.stringify(error));
+                    console.log(error);
+                }
+            });
+        }
+    }
+
     render() {
         return (
             <div className="block">
