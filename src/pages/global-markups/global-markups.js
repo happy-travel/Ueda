4bbd4936c09c3c45f3c66a6dc0dc742d5936454d
Ходжаян Артём Геннадиевич <icegroovekid@gmail.com>
2021-05-04 11:38:14 +0500

    Global markups bugfix

--- src/pages/global-markups/global-markups.js
+++ src/pages/global-markups/global-markups.js
@@ -3,17 +3,16 @@ import apiMethods from '../../core/methods';
 import Markups from 'matsumoto/src/parts/markups/markups';
 
 const GlobalMarkups = () => (
-
-        <div className="global-markups">
-            <section style={{ marginTop: '150px' }}>
-                <Markups
-                    id={null}
-                    emptyText={'No markups'}
-                    markupsRoute={apiMethods.markupsGlobal }
-                    markupRoute={apiMethods.markupGlobal }
-                />
-            </section>
-        </div>
+    <div className="global-markups">
+        <section style={{ marginTop: '150px' }}>
+            <Markups
+                id={null}
+                emptyText={'No markups'}
+                markupsRoute={apiMethods.markupsGlobal }
+                markupRoute={apiMethods.markupGlobal }
+            />
+        </section>
+    </div>
 );
 
 export default GlobalMarkups;
