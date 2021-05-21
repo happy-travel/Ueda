b42612cf0d4b1ce6f3d4753c52ad9e65d2a13191
Ходжаян Артём Геннадиевич <icegroovekid@gmail.com>
2021-05-21 16:22:44 +0500

    Added UI for getting and managing  agent system settings

--- src/pages/agency/search-options-form.js
+++ src/pages/agency/search-options-form.js
@@ -1,58 +1,197 @@
-import React from 'react';
-import { CachedForm, FieldSelect, FieldSwitch } from 'matsumoto/src/components/form';
+import React, { useState, useEffect } from 'react';
+import { CachedForm, FieldCheckbox, FieldSelect, FieldText } from 'matsumoto/src/components/form';
+import { API } from 'matsumoto/src/core';
+import apiMethods from 'core/methods';
 
-class SearchOptionsForm extends React.Component {
-    render() {
-        const { initialValues, onSubmit } = this.props;
-        return (
+const SearchOptionsForm = ({ id }) => {
+    const [settings, setSettings] = useState(null);
+    const [defaultSettings, setDefaultSettings] = useState(true);
+
+    useEffect(() => {
+        API.get({
+            url: apiMethods.availabilitySearchOptions(id),
+            success: (settings) => {
+                setSettings(settings);
+            }
+        });
+    }, [])
+
+    const submitSettings = (values) => {
+        if(defaultSettings) {
+            API.put({
+                url: apiMethods.availabilitySearchOptions(id),
+                body: values,
+            });
+        } else {
+            API.delete({
+                url: apiMethods.availabilitySearchOptions(id),
+            });
+        }
+    }
+
+    const changeHandler = (value) => {
+        setDefaultSettings(value)
+    }
+
+    return (
+        <div style={{ marginBottom: '50px' }}>
+            {settings?.enabledSuppliers &&
             <CachedForm
-                initialValues={
-                    initialValues || {
-                        enabledSuppliers: {}
-                    }
-                }
-                enableReinitialize
-                onSubmit={onSubmit}
+                initialValues={settings}
+                onSubmit={submitSettings}
                 render={(formik) => (
                     <div className="form">
-                        <div className="row"><FieldSwitch formik={formik} id="isMarkupDisabled" label="Is Markup Disabled" /></div>
-                        <div className="row"><FieldSwitch formik={formik} id="isSupplierVisible" label="Is Supplier Visible" /></div>
+                        <h1>Agency Settings</h1>
                         <div className="row">
-                            <FieldSelect formik={formik}
-                                         id="aprMode"
-                                         label="APR Mode"
-                                         options={[
-                                             { value: '', text: 'None' },
-                                             { value: 'Hide', text: 'Hide' },
-                                             { value: 'DisplayOnly', text: 'Display Only' },
-                                             { value: 'CardPurchasesOnly', text: 'Card Purchases Only' },
-                                             { value: 'CardAndAccountPurchases', text: 'Card And Account Purchases' }
-                                         ]}
+                            <FieldCheckbox
+                                formik={formik}
+                                id="defaultSettings"
+                                value={defaultSettings}
+                                onChange={changeHandler}
+                                label="Default settings"
                             />
                         </div>
-                        <div className="row">
-                            <FieldSelect formik={formik}
-                                         id="passedDeadlineOffersMode"
-                                         label="Passed Deadline Offers Mode"
-                                         options={[
-                                             { value: '', text: 'None' },
-                                             { value: 'Hide', text: 'Hide' },
-                                             { value: 'DisplayOnly', text: 'Display Only' },
-                                             { value: 'CardPurchasesOnly', text: 'Card Purchases Only' },
-                                             { value: 'CardAndAccountPurchases', text: 'Card And Account Purchases' }
-                                         ]}
-                            />
-                        </div>
-                        <div className="row">
-                            <label>Enabled Suppliers</label>
-                        </div>
-                        <div className="row"><FieldSwitch formik={formik} id="enabledSuppliers.Unknown" label="Unknown" /></div>
-                        <div className="row"><FieldSwitch formik={formik} id="enabledSuppliers.Netstorming" label="Netstorming" /></div>
-                        <div className="row"><FieldSwitch formik={formik} id="enabledSuppliers.Illusions" label="Illusions" /></div>
-                        <div className="row"><FieldSwitch formik={formik} id="enabledSuppliers.DirectContracts" label="DirectContracts" /></div>
-                        <div className="row"><FieldSwitch formik={formik} id="enabledSuppliers.Etg" label="Etg" /></div>
-                        <div className="row"><FieldSwitch formik={formik} id="enabledSuppliers.Rakuten" label="Rakuten" /></div>
-                        <div className="row submit-holder">
+                        {!defaultSettings &&
+                        <div className="row double">
+                            <div style={{ marginRight: '300px' }} className="modes">
+                                <h1>Apr Mode</h1>
+                                <div className="row">
+                                    <FieldSelect formik={formik}
+                                                 id="AprMode"
+                                                 label="Apr Mode"
+                                                 options={[
+                                                     { value: 'Hide', text: 'Hide' },
+                                                     { value: 'DisplayOnly', text: 'Display Only' },
+                                                     { value: 'CardPurchasesOnly', text: 'Card Purchases Only' },
+                                                     { value: 'CardAndAccountPurchases', text: 'Card And Account Purchases' }
+                                                 ]}
+                                    />
+                                </div>
+                                <h1>Passed Deadline Offers Mode</h1>
+                                <div className="row">
+                                    <FieldSelect formik={formik}
+                                                 id="PassedDeadlineOffersMode"
+                                                 label="Passed Deadline Offers Mode"
+                                                 options={[
+                                                     { value: 'Hide', text: 'Hide' },
+                                                     { value: 'DisplayOnly', text: 'Display Only' },
+                                                     { value: 'CardPurchasesOnly', text: 'Card Purchases Only' },
+                                                     { value: 'CardAndAccountPurchases', text: 'Card And Account Purchases' }
+                                                 ]}
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldText
+                                        formik={formik}
+                                        id="customDeadlineShift"
+                                        label="Сustom Deadline Shift"
+                                        maxLength={2}
+                                        numeric
+                                    />
+                                </div>
+                            </div>
+                            <div className="suppliers">
+                                <h1>Enabled suppliers</h1>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.Columbus"
+                                        value={settings?.enabledSuppliers?.Columbus}
+                                        label="Columbus"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.DirectContracts"
+                                        value={settings?.enabledSuppliers?.DirectContracts}
+                                        label="Direct Contracts"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.Etg"
+                                        value={settings?.enabledSuppliers?.Etg}
+                                        label="Etg"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.Illusions"
+                                        value={settings?.enabledSuppliers?.Illusions}
+                                        label="Illusions"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.Jumeirah"
+                                        value={settings?.enabledSuppliers?.Jumeirah}
+                                        label="Jumeirah"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.Netstorming"
+                                        value={settings?.enabledSuppliers?.Netstorming}
+                                        label="Netstorming"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.Rakuten"
+                                        value={settings?.enabledSuppliers?.Rakuten}
+                                        label="Rakuten"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.TravelgateXTest"
+                                        value={settings?.enabledSuppliers?.TravelgateXTest}
+                                        label="TravelgateXTest"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.IsDirectContractFlagVisible"
+                                        value={settings?.enabledSuppliers?.IsDirectContractFlagVisible}
+                                        label="Direct Contract Flag View"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.IsMarkupDisabled"
+                                        value={settings?.enabledSuppliers?.IsMarkupDisabled}
+                                        label="Markup Disabled"
+                                    />
+                                </div>
+                                <div className="row">
+                                    <FieldCheckbox
+                                        formik={formik}
+                                        id="enabledSuppliers.isSupplierVisible"
+                                        formik={formik}
+                                        id="supplierVisible"
+                                        value={settings?.enabledSuppliers?.isSupplierVisible}
+                                        label="Supplier Visible View"
+                                    />
+                                </div>
+                            </div>
+                        </div>}
+                        <div
+                            className="row submit-holder"
+                            style={{
+                            marginTop: '50px',
+                                width: '33%',
+                                marginLeft: '25%'
+                            }}
+                            >
                             <div className="field">
                                 <div className="inner">
                                     <button type="submit" className="button">
@@ -63,9 +202,9 @@ class SearchOptionsForm extends React.Component {
                         </div>
                     </div>
                 )}
-            />
-        )
-    }
+            />}
+        </div>
+    )
 }
 
 export default SearchOptionsForm;
